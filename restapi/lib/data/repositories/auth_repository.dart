621f93d59b17f6e2d23577c9b98d158542e160f3
SASCHADDD <saschadanu23@gmail.com>
2026-05-02 01:00:12 +0700

    menambahkan kelas AuthRepository untuk mengelola autentikasi dan penyimpanan token

---
+++ restapi/lib/data/repositories/auth_repository.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AuthRepository{
+  final String baseUrl = "https://ternak-be-production.up.railway.app/api/v1";
+  final _storage = const FlutterSecureStorage();
+}
