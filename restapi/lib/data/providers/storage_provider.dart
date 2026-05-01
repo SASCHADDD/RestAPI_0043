232bcac8f1d59d61c917010c4f5d2d804debe2c6
SASCHADDD <saschadanu23@gmail.com>
2026-05-02 00:54:15 +0700

    update dependencies and remove unused packages from pubspec.yaml and pubspec.lock add import statement for flutter_secure_storage in StorageProvider remove DeviceInfoPlusMacosPlugin registration from GeneratedPluginRegistrant

--- restapi/lib/data/providers/storage_provider.dart
+++ restapi/lib/data/providers/storage_provider.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
 class StorageProvider {
   final _storage = const FlutterSecureStorage();
   static const _tokenKey = 'jwt_token';
