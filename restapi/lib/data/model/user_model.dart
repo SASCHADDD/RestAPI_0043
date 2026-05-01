96eab91f78d7505f921d2008b9a9c51c361c2b8c
SASCHADDD <saschadanu23@gmail.com>
2026-05-02 00:44:11 +0700

    memperbaiki kesalahan sintaks pada factory method fromJson di kelas UserModel

--- restapi/lib/data/model/user_model.dart
+++ restapi/lib/data/model/user_model.dart
@@ -10,7 +10,7 @@ class UserModel extends Equatable {
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json){
-    return UserModel(id: json ['id']??,
+    return UserModel(id: json ['id']??'',
      username: json['username']??''
      );
   }
