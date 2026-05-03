5a6ef7712c6ee9e0c97b1d675c0ca4db87db282f
SASCHADDD <saschadanu23@gmail.com>
2026-05-03 17:34:14 +0700

    memperbaiki konstruktor pada LoginRequested dan RegisterRequested untuk menggunakan parameter posisi

--- restapi/lib/logic/bloc/Auth/auth_event.dart
+++ restapi/lib/logic/bloc/Auth/auth_event.dart
@@ -8,12 +8,12 @@ abstract class AuthEvent extends Equatable{
 class AppStarted extends AuthEvent {}
 class LoginRequested extends AuthEvent {
   final String email, password;
-  LoginRequested({required this.email, required this.password});
+  LoginRequested( this.email,  this.password);
 }
 
 class RegisterRequested extends AuthEvent {
   final String name, email, password;
-  RegisterRequested({required this.name, required this.email, required this.password});
+  RegisterRequested(this.name,  this.email, this.password);
 }
 
 class LogoutRequested extends AuthEvent {}
